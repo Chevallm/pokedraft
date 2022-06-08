1ce175b4ed3324e9165bde45d1a36ad282ffe12f
Chevallm <maxime.chevallier_pichon@yahoo.fr>
2022-06-09 01:56:34 +0200

    4 files modified

--- index.ts
+++ index.ts
@@ -3,10 +3,27 @@ import { POKEMONS } from './src/pokemons';
 import { UIManager } from './src/ui-manager';
 import './style.css';
 
+const player = {};
+
 // Write TypeScript code!
 const uiManager = new UIManager();
 
 const pickRef = document.getElementsByClassName('pick')[0];
-for (let i = 0; i < 151; i++) {
-  pickRef.appendChild(uiManager.createPokemonCard(POKEMONS[i]));
+//pickRef.appendChild(uiManager.createPokemonCard(POKEMONS[i]));
+
+// start
+pick();
+
+function pick() {
+  showPickView();
+}
+
+function showPickView() {
+  const ref = document.getElementsByClassName('pick')[0];
+  for (let i = 0; i < 3; i++) {
+    const rand = Math.floor(Math.random() * (151 - 0)) + 0;
+    const pokemon = POKEMONS[rand];
+    const element = uiManager.createPokemonCard(pokemon);
+    ref.appendChild(element);
+  }
 }
--- src/ui-manager.ts
+++ src/ui-manager.ts
@@ -1,31 +1,53 @@
 import { Pokemon } from './pokemons';
 
+const typeColors = {
+  Grass: '#68BC2D',
+  Fire: '#C72100',
+  Water: '#2F91F2',
+  Bug: '#9EAD19',
+  Normal: '#C7C2B9',
+  Poison: '#8E408F',
+  Electric: '#F9B616',
+  Ground: '#D1B055',
+  Fairy: '#F6B2F6',
+  Flying: '#92A5F2',
+  Fighting: '#81341D',
+  Psychic: '#ED4882',
+  Rock: '#B69E54',
+  Steel: '#B3B3C2',
+  Ice: '#97E3FB',
+  Ghost: '#5F5FB1',
+};
+
 export class UIManager {
   constructor() {}
 
   createPokemonCard(pokemon: Pokemon) {
     const d = document.createElement('div');
-    d.innerHTML = `
-    <div class="pokemon-pick">
-        <img
-          src="https://stackblitz.com/files/typescript-qsqwua/github/Chevallm/pokedraft/master/images/${pokemon.pokedexId}.png"
+    d.innerHTML = `<div class="pokemon-pick" style="background-image: linear-gradient(110deg, ${
+      typeColors[pokemon.type1]
+    } 50%, ${typeColors[pokemon.type2 || pokemon.type1]} 50%);">
+        <img class="pokeimage"
+          src="https://stackblitz.com/files/typescript-qsqwua/github/Chevallm/pokedraft/master/images/${
+            pokemon.pokedexId
+          }.png"
         />
         <dt>Nom</dt>
         <dd>${pokemon.nom}</dd>
-        <dt>Stats</dt>
-        <dt>pv</dt>
+        <dt>PV</dt>
         <dd>${pokemon.pv}</dd>
-        <dt>att</dt>
+        <dt>Attaque</dt>
         <dd>${pokemon.att}</dd>
-        <dt>def</dt>
+        <dt>Défense</dt>
         <dd>${pokemon.def}</dd>
-        <dt>attspé</dt>
+        <dt>Attaque Spéciale</dt>
         <dd>${pokemon.attspe}</dd>
-        <dt>defspé</dt>
+        <dt>Défense Spéciale</dt>
         <dd>${pokemon.defspe}</dd>
+        <br/>
+        <button>Pick</button>
       </div>
     `;
-    d.style.backgroundColor = '';
     return d;
   }
 }
